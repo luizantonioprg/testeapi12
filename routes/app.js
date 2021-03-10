c1b9a8260f237f3429e5ede54e9fd949574c8142
Luiz Antônio <luiz.antonio.contato@outlook.com>
2021-03-10 00:02:28 -0300

    dwa
    dwa

--- routes/app.js
+++ routes/app.js
@@ -1,18 +1,27 @@
 const instaTouch = require('instatouch');
 const express = require('express');
 const router = express.Router();
+const ProxyList = require('free-proxy');
+const proxyList = new ProxyList();
 
 router.get('/:id', (req, res) => {
-  (async () => {
-    try {
-      const options = { count: 100 };
-      const comments = await instaTouch.comments(req.params.id, options);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      return res.send(comments);
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+  proxyList
+    .get()
+    .then(function (proxies) {
+      (async () => {
+        try {
+          const options = { count: 100 };
+          const comments = await instaTouch.comments(req.params.id, options);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          return res.send(comments);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    })
+    .catch(function (error) {
+      throw new Error(error);
+    });
 });
 
 router.get('/', (req, res) => {
